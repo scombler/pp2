a97f62c60092cf77a59572323a5182aa352b64d1
scombler <scombler@mai.ru>
2023-03-03 18:13:09 +0600

    done

--- tsis3/generators/4.py
+++ tsis3/generators/4.py
@@ -2,6 +2,10 @@ def squares(x, y):
     for nums in range(x, y + 1):
         yield nums ** 2
 
-x, y = map(int, input().split())
+#x, y = map(int, input().split())
+#print(*squares(x, y))
+x = int(input())
+y = int(input())
 
-print(*squares(x, y))
+for i in squares(x, y):
+    print(i)
